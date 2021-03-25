8449055370cf98156bc56bad28c05fa40bc1dd55
BANGHO <BANGHO@DESKTOP-J31T5DC>
2021-03-25 16:01:06 -0300

    Inicialización del Proyecto

---
+++ proyecto02-25-3/src/main/java/ar/edu/unju/edm/Proyecto02253Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto02253Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto02253Application.class, args);
+	}
+
+}
